a8e73ef5238ec2f3d3ff98d551914c2952ef148e
Yuunai <minta96@gmail.com>
2018-01-09 20:17:03 +0100

    Task I added!

---
+++ ZadI/ZadI/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace ZadI
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        private void loadImage(Object stateInfo)
+        {
+            string fileName = (string)((object[])stateInfo)[0];
+            this.Dispatcher.Invoke(() =>
+            {
+                bitmap.Source = new BitmapImage(new Uri(fileName));
+            });
+
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                ThreadPool.QueueUserWorkItem(loadImage, new object[] { op.FileName});
+            }
+        }
+    }
+}
